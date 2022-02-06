36c0d3b67d02d3764a863d244913bbffb5240708
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-02-05 18:20:56 -0800

    Create rois.sh

---
+++ rois.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=rois_bruker
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=trc
+#SBATCH --cpus-per-task=2
+#SBATCH --output=./roi-logs/mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+
+date
+python3 -u /home/users/asmart/projects/PER-processing/read_rois.py
