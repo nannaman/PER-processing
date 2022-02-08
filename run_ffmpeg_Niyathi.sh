269037a03c179c2160782afe077acb10e24eef02
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-02-08 10:42:26 -0800

    Create run_ffmpeg_Niyathi.sh

---
+++ run_ffmpeg_Niyathi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=NA_ffmpeg
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=trc
+##two should comemnt out SBATCH --mem 260G
+#SBATCH --cpus-per-task=2
+#SBATCH --output=./logs/mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
+
+ml system
+ml ffmpeg
+
+ml python/3.6.1
+
+date
+python3 -u /home/users/asmart/projects/PER-processing/ffmpeg_Niyathi.py
