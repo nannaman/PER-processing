480722d331a6a2ef9bcff75b06857644cc14d3a3
nannaman <99289999+nannaman@users.noreply.github.com>
2022-02-14 16:12:05 -0800

    Update ffmpeg_Niyathi.py

--- ffmpeg_Niyathi.py
+++ ffmpeg_Niyathi.py
@@ -11,29 +11,39 @@ video_2_name = None  #change if have two videos for the same day
 
 #check to make sure this is right, but should work as long as your file structure doesn't change
 video_1_path = '/oak/stanford/groups/trc/data/Niyathi/' + str(date) +'/'   #path to video 1, should end in /
-video_2_path = None #change if have two videos
+video_2_path = None #'/oak/stanford/groups/trc/data/Niyathi/' + str(date) +'/' #change if have two videos
 
 #should not need to change these as long as the video_1_path is correct
 video_1_jpeg_path = os.path.join(video_1_path, 'analysis/Video_1/')     #path where should save jpegs should end in /
-video_2_jpeg_path = None  #change if have two videos
+video_2_jpeg_path = None  #os.path.join(video_1_path, 'analysis/Video_2/') #change if have two videos
 
 def main():
   #make jpeg directories
   make_dirs(video_1_jpeg_path)
-  if video_2_jpeg_path is not None: 
+  if video_2_name is not None: 
     make_dirs(video_2_jpeg_path)
   
   #run ffmpeg code
   final_jpeg_path_v1 = os.path.join(video_1_jpeg_path, 'V01frame_%07d.jpg')
   final_vid_1_path = os.path.join(video_1_path, video_1_name)
-  cmd = f'ffmpeg -i {final_vid_1_path} {final_jpeg_path_v1}'
-  subprocess.call(cmd.split(" "))
+  cmd = f'ffmpeg -i REPLACEPATH REPLACEPATHJPEG'  #have to do it this way because I have spaces in the filepath
+  split_version = cmd.split(" ")
+  split_version[2] = final_vid_1_path
+  split_version[3] = final_jpeg_path_v1
+  subprocess.call(split_version)
+  
+  print('video 1 DONE')
   
   if video_2_name is not None:
     final_jpeg_path_v2 = os.path.join(video_2_jpeg_path, 'V02frame_%07d.jpg')
     final_vid_2_path = os.path.join(video_2_path, video_2_name)
-    cmd = f'ffmpeg -i {final_vid_2_path} {final_jpeg_path_v2}'
-    subprocess.call(cmd.split(" "))
+    cmd = f'ffmpeg -i REPLACEPATH REPLACEPATHJPEG'  #have to do it this way because I have spaces in the filepath
+    split_version = cmd.split(" ")
+    split_version[2] = final_vid_2_path
+    split_version[3] = final_jpeg_path_v2
+    subprocess.call(split_version)
+    
+    print('video 2 DONE')
   
   
   
