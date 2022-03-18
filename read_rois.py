1f1d691c30e10d88bcf4cf062bba601fa6ee2fc1
nannaman <99289999+nannaman@users.noreply.github.com>
2022-03-18 10:55:00 -0700

    Update read_rois.py

--- read_rois.py
+++ read_rois.py
@@ -24,7 +24,7 @@ from read_roi import read_roi_zip
 
 
 #### change this ####
-date = '1-5-22'  #name of folder the files are in 
+date = '1-3-22'  #name of folder the files are in 
 video_number = 1   # video number using
 roi_number = 1 #make sure rois are saved with this filename format "RoiSet_vid1"
 path_to_date = '/oak/stanford/groups/trc/data/Niyathi/' ##end with / and just before date folder (shouldn't need to change)
