3c4004d075d31a5017833bdc4fa624dc3b1ce945
nannaman <99289999+nannaman@users.noreply.github.com>
2022-03-23 19:41:55 -0700

    Update run_rois.sh

--- run_rois.sh
+++ run_rois.sh
@@ -3,8 +3,12 @@
 #SBATCH --time=4-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=trc
+#SBATCH --cpus-per-task=2
+#SBATCH --output=./roi-logs/mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
 
-ml python/3.6.1
+ml python/3.8.12
 
 date
 python3 -u /home/users/nannaman/projects/PER-processing/read_rois.py
