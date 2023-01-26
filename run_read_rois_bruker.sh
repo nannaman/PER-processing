f76a507c110ddea4e35d54c80e6f9d01a6e4c32d
nannaman <99289999+nannaman@users.noreply.github.com>
2023-01-26 12:38:24 -0800

    Update run_read_rois_bruker.sh

--- run_read_rois_bruker.sh
+++ run_read_rois_bruker.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=NA_ffmpeg
+#SBATCH --job-name=read_rois
 #SBATCH --time=4-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=trc
-##two should comemnt out SBATCH --mem 260G
 #SBATCH --cpus-per-task=2
-#SBATCH --output=./logs/mainlog.out
+#SBATCH --output=./roi-logs/mainlog.out
 #SBATCH --open-mode=append
-#SBATCH --mail-type=ALLml 
+#SBATCH --mail-type=ALL
 
 python/3.6.1
 ml py-numpy/1.14.3_py36
