2cc4d6626557fc8216255219fb40a9b64acfe162
nannaman <99289999+nannaman@users.noreply.github.com>
2023-01-26 17:24:58 -0800

    Update run_read_rois_bruker.sh

--- run_read_rois_bruker.sh
+++ run_read_rois_bruker.sh
@@ -8,7 +8,7 @@
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
 
-python/3.6.1
-ml py-numpy/1.19.5_py36
+ml python/3.6.1
+ml py-numpy/1.14.3_py36
 date
 python3 -u /home/users/nannaman/projects/PER-processing/read_rois_bruker.py
